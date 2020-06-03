f694de445377f3dc40c4717b0cd48eda0f1248f9
Mario Dujić <liberasoftware@gmail.com>
2020-06-03 10:27:34 +0200

    removed unnecessary parameter

--- src/thoughts/services/LocalReader.py
+++ src/thoughts/services/LocalReader.py
@@ -10,7 +10,7 @@ class LocalReader(Read):
     def read(self, *args):
         thoughts = list()
         with open(Environment.get().LOCAL_THOUGHT_PATH, encoding="utf8") as json_file:
-            thoughts_data = json.load(json_file, encoding="utf8")
+            thoughts_data = json.load(json_file)
             for t in thoughts_data:
                 thought = Thought.json_to_object(t)
                 thoughts.append(thought)
