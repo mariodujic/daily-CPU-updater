1653fccef26b3355f0d6ed7e32864340cf617b6f
Mario Dujić <liberasoftware@gmail.com>
2020-06-11 12:19:21 +0200

    removed unnecessary line

--- App.py
+++ App.py
@@ -2,5 +2,4 @@ from src.Main import Main
 from src.environment import Environment
 from src.environment.EnvironmentType import EnvironmentType
 
-Environment.t = EnvironmentType.PRODUCTION
 Main().initialize()
